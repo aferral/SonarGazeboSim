e637f22d19b14388ca15ba5a4d3910585c232927
andres <andreslago5@hotmail.com>
2017-04-26 01:35:30 -0300

    Now with quad conversion for binary image :

--- src/newVer.py
+++ src/newVer.py
@@ -9,7 +9,7 @@ import base64
 #define constants
 
 #All vars are in meters
-
+from gridUtils import rectangularize
 
 wallWidth = 0.4
 
@@ -123,10 +123,11 @@ def createSimGrid(msg):
 		elem.drawOnImage(canvas)
 
 	print "New origin at ",(-minX + 5)," ",(-minY + 5)," IMAGE CORDS"
-	cv2.imwrite("out"+".png", canvas)
+	cv2.imwrite("outCanvas"+".png", canvas)
 
 	#Now export all this info to json for simulation
 
+	rectangularize(canvas)
 	encoded = base64.b64encode(open('out.png','rb').read())
 	
 	d = {}
